cc6c2a0e947ade7aad7b46fe83ea1e7d0df461a4
abhishekp906 <abhishek.p906@gmail.com>
2013-03-28 23:22:22 +0530

    devise and bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 GrepCom::Application.routes.draw do
+  resources :profiles
+
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ GrepCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'profiles#index'
 
   # See how all your routes lay out with "rake routes"
 
