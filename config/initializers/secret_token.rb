766a00b53b96b4802492fe3ca8b290c718b925e1
abhishekp906 <abhishek.p906@gmail.com>
2013-03-28 19:42:14 +0530

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrepCom::Application.config.secret_token = '070bc28fd4265ded945cbd1009d5e3934ed576c77169e0d4a3717b08c61bf657cb5dfb8b4f9a79292a0cead898d06ee5dd96fecebae19f49fde30b38010fdb2b'
